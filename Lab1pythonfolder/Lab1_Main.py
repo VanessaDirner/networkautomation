40f121abe13b3ea4fd5d4aae0f2a0777fc09a29a
VanessaDirner <vanessadirner.work@gmail.com>
2025-09-28 13:55:40 -0600

    working on ip parsing & validation

--- Lab1pythonfolder/Lab1_Main.py
+++ Lab1pythonfolder/Lab1_Main.py
@@ -1,14 +1,16 @@
+
+## import modules
+import module1
+import module2
+import module3
+
+
 ## This is an introduction to the program, purpose, author etc.
 print("Welcome to my program")
 print("A wonderful place for network enthusiasts. This lab 1 assignment will help you with many administrative tasks.")
 print("This program was written by Vanessa Dirner on September 23, 2025.")
 print("Let's get started")
 
-
-## import modules
-import module1
-import module2
-import module3
 
 
 ## direct user to make a choice
@@ -25,18 +27,18 @@ menuchoice = input()
 
 print("Your input was " + menuchoice)
 ## exit option
-quit = bool(0)
+#quit = bool(0)
 
-while (quit != 1):
-    match menuchoice:
-        case "1":
-            print("Starting menu choice 1 - ‘Validate an IP Address and a Subnet Mask’")
-            module1.validateip()
-        case "2":
-            print("Starting menu choice 2 -  ‘Generate Cisco Router Configuration’")
-            module2.routerconfig()
-        case "3":
-            print("Starting menu choice 3 - ‘Analyze a “show interface brief” command output’")
-            module3.analysis()
+#while (quit != 1):
+match menuchoice:
+    case "1":
+        print("Starting menu choice 1 - ‘Validate an IP Address and a Subnet Mask’")
+        module1.validateip()
+    case "2":
+        print("Starting menu choice 2 -  ‘Generate Cisco Router Configuration’")
+        module2.routerconfig()
+    case "3":
+        print("Starting menu choice 3 - ‘Analyze a “show interface brief” command output’")
+        module3.analysis()
 
 
