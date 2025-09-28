4759e5d61932375e8650c0208eaded76fd1168e0
VanessaDirner <vanessadirner.work@gmail.com>
2025-09-28 16:58:41 -0600

    working on module 2 base info + modularize module 1 (haha)

--- Lab1pythonfolder/module1.py
+++ Lab1pythonfolder/module1.py
@@ -1,58 +1,60 @@
+## reference: https://stackoverflow.com/questions/63192480/check-if-valid-ip-address-from-user-input
+def validateIPAddress():
+    valid = False
+    retries = int(0)
+    quit = False
+    while valid == False and retries < 3 and quit != True:
+        try:
+            address = ipaddress.ip_address(input('Enter IP address: '))
+            print(address)
+        except ValueError:
+            print(ValueError.with_traceback)
+            print("Invalid value. Try again.")
+            retries = retries + 1
+            print("retries", retries)
+            if ipaddr == "quit":
+                print("quit was entered. Quitting..")
+                quit = True
+            elif retries >= 3:
+                print("3 tries occured. Quitting ip address validation")
+        else:
+            print("Valid IP address")
+            valid = True
+            return address
+
+def validateSubnet():
+    valid = False
+    retries = int(0)
+    quit = False
+    while valid == False and retries < 3 and quit != True:
+        try:
+            subnet = ipaddress.ip_network(input('Enter your network address and subnet in CIDR notation: '))
+            print(subnet)
+        except ValueError:
+            print(ValueError.with_traceback)
+            print("Invalid value. Try again. A valid subnet example would be 10.1.1.0 /24")
+            retries = retries + 1
+            print("retries", retries)
+            if ipaddr == "quit":
+                print("quit was entered. Quitting..")
+                quit = True
+            elif retries >= 3:
+                print("3 tries occured. Quitting ip network validation")
+        else:
+            print("Valid subnet")
+            valid = True
+            return subnet
+
+## reference: https://stackoverflow.com/questions/819355/how-can-i-check-if-an-ip-is-in-a-network-in-python
+def compareIpAddresstoSubnet():
+    isipinnetwork = ipaddress.ip_address(ipaddr) in ipaddress.ip_network(ipnetw)
+    return isipinnetwork
+
 def ipmodule():
     import ipaddress, Lab1_Main, module2, module3
     ## definitions
 
-    ## reference: https://stackoverflow.com/questions/63192480/check-if-valid-ip-address-from-user-input
-    def validateIPAddress():
-        valid = False
-        retries = int(0)
-        quit = False
-        while valid == False and retries < 3 and quit != True:
-            try:
-                address = ipaddress.ip_address(input('Enter IP address: '))
-                print(address)
-            except ValueError:
-                print(ValueError.with_traceback)
-                print("Invalid value. Try again.")
-                retries = retries + 1
-                print("retries", retries)
-                if ipaddr == "quit":
-                    print("quit was entered. Quitting..")
-                    quit = True
-                elif retries >= 3:
-                    print("3 tries occured. Quitting ip address validation")
-            else:
-                print("Valid IP address")
-                valid = True
-                return address
 
-    def validateSubnet():
-        valid = False
-        retries = int(0)
-        quit = False
-        while valid == False and retries < 3 and quit != True:
-            try:
-                subnet = ipaddress.ip_network(input('Enter your network address and subnet in CIDR notation: '))
-                print(subnet)
-            except ValueError:
-                print(ValueError.with_traceback)
-                print("Invalid value. Try again. A valid subnet example would be 10.1.1.0 /24")
-                retries = retries + 1
-                print("retries", retries)
-                if ipaddr == "quit":
-                    print("quit was entered. Quitting..")
-                    quit = True
-                elif retries >= 3:
-                    print("3 tries occured. Quitting ip network validation")
-            else:
-                print("Valid subnet")
-                valid = True
-                return subnet
-    
- ## reference: https://stackoverflow.com/questions/819355/how-can-i-check-if-an-ip-is-in-a-network-in-python
-    def compareIpAddresstoSubnet():
-        isipinnetwork = ipaddress.ip_address(ipaddr) in ipaddress.ip_network(ipnetw)
-        return isipinnetwork
 
 
     print("entered module1")
@@ -65,6 +67,7 @@ def ipmodule():
 
     ## process first octet
     print("Enter the ip address in dotted decimal notation format now:")
+    ## for example, your current IP address and subnet mask is: -> read host information
 
     ## declare variable for ip address with type of ipaddress
     ipaddr = ipaddress.ip_address
--- Lab1pythonfolder/module2.py
+++ Lab1pythonfolder/module2.py
@@ -1,16 +1,28 @@
 
 def routerconfig():
     print("entered module2")
-
+    
     ## This is an introduction to the program, purpose, author etc.
     print("Welcome to module 2 - ‘Generate Cisco Router Configuration’ ")
     
-    ## Prompt the user for a hostname
-    ## Validate the hostname given
-    ## Save the hostname configuration
+    configuration = {
+        "hostname": hostname,
+        "IP Address": ipaddr
+    }
 
+    ## Prompt the user for a hostname
+    print("Please enter a hostname:")
+    hostname = input()
+    ## Validate the hostname given
+    print("You've entered", hostname , "as your hostname")
+    ## Save the hostname configuration
+    exit()
+    
     ## Prompt the user for the IP Address
+    ipaddr = input()
     ## Validate the IP address given
+    import module1
+    module1.validateIPAddress()
     ## Save the IP address configuration
 
     ## Prompt the user for the subnet mask
@@ -18,5 +30,8 @@ def routerconfig():
     ## Save the subnet mask configuration
 
     ## Tell the user the details of the saved configuration
+    
+    ## do you want to add more configuration? prompt for type of input and value, add to list, save information
+
     ## Go back to main menu
 
