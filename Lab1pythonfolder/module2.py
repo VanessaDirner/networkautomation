a604bf57e2d8c53d4624f6b6f6836e6e08a2d00e
VanessaDirner <vanessadirner.work@gmail.com>
2025-09-28 17:40:33 -0600

    working on module 3

--- Lab1pythonfolder/module2.py
+++ Lab1pythonfolder/module2.py
@@ -1,3 +1,5 @@
+import os
+import getpass
 
 def routerconfig():
     print("entered module2")
@@ -6,8 +8,6 @@ def routerconfig():
     print("Welcome to module 2 - ‘Generate Cisco Router Configuration’ ")
     
     configuration = {
-        "hostname": hostname,
-        "IP Address": ipaddr
     }
 
     ## Prompt the user for a hostname
@@ -16,13 +16,20 @@ def routerconfig():
     ## Validate the hostname given
     print("You've entered", hostname , "as your hostname")
     ## Save the hostname configuration
-    exit()
+    configuration.update({"hostname": hostname})
+    print(configuration, " configuration updated.")
+
     
     ## Prompt the user for the IP Address
+    print("please enter an ip address")
     ipaddr = input()
+
+    configuration.update({"IP Address": ipaddr})
+    print(configuration, "you've added", configuration.get("IP Address"), "as your IP address")
+
     ## Validate the IP address given
-    import module1
-    module1.validateIPAddress()
+ #   import module1
+#    module1.validateIPAddress()
     ## Save the IP address configuration
 
     ## Prompt the user for the subnet mask
@@ -30,8 +37,33 @@ def routerconfig():
     ## Save the subnet mask configuration
 
     ## Tell the user the details of the saved configuration
-    
+    print("your final configuration details are:", configuration)
+        
+    ## save all details to a file
+    filename = "CiscoRouterConfig.txt"
+
+    print("saving configuration to ", filename)
+
+    with open(filename, "a") as f:
+        f.write(configuration.get("hostname"))
+
     ## do you want to add more configuration? prompt for type of input and value, add to list, save information
 
     ## Go back to main menu
+
+
+'''
+    print("where do you want to save the file?")
+
+    home = os.path.expanduser("")
+    environment = os.defpath
+    print(home, environment)
+    exit()
+
+
+    path = input()
+
+    fullpath = home + "CiscoRouterConfiguration.txt"
+
+'''
 
--- Lab1pythonfolder/module3.py
+++ Lab1pythonfolder/module3.py
@@ -6,10 +6,23 @@ def analysis():
     print("Welcome to module 3 - ‘Analyze a “show interface brief” command output’ ")
     
     ## Look for ‘show interface brief’ file
+ 
     ## 	If it does not exist at the location expected/specified, prompt user for location
+
     ## Read details of show interface brief
-    ## Ask whether they want a specific interface or all 
-    ## If they ask for all, print out the details of all
-    ## If they specify a specific interface, print that one out
-    ## Ask if they want to print out a specific interface or all, or quit 
-    ## If they say quit, exit option 3. Otherwise go to the beginning of option 3.
+    with open("showipconfig.txt") as file:
+        print(file.read())
+        ## Ask whether they want a specific interface or all 
+        print("Would you like to review a specific interface or see all?")
+        response = input()
+        ## If they ask for all, print out the details of all
+        if response == "all":
+            print(file.read())
+        ## If they specify a specific interface, print that one out
+        else:
+            print(file.read(5))
+
+        ## Ask if they want to print out a specific interface or all, or quit 
+        
+        ## If they say quit, exit option 3. Otherwise go to the beginning of option 3.
+        file.close()
