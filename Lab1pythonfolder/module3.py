6be06c77662e16393687002a60a5e32977fecf56
VanessaDirner <vanessadirner.work@gmail.com>
2025-09-28 19:32:24 -0600

    module 3 read lines

--- Lab1pythonfolder/module3.py
+++ Lab1pythonfolder/module3.py
@@ -1,3 +1,4 @@
+import re
 
 def analysis():
     print("entered module3")
@@ -10,19 +11,67 @@ def analysis():
     ## 	If it does not exist at the location expected/specified, prompt user for location
 
     ## Read details of show interface brief
-    with open("showipconfig.txt") as file:
-        print(file.read())
-        ## Ask whether they want a specific interface or all 
-        print("Would you like to review a specific interface or see all?")
-        response = input()
-        ## If they ask for all, print out the details of all
-        if response == "all":
-            print(file.read())
-        ## If they specify a specific interface, print that one out
-        else:
-            print(file.read(5))
+    with open("./showipconfig.txt") as file:
 
-        ## Ask if they want to print out a specific interface or all, or quit 
         
+        ## parse the file
+        ## separate out the lines
+        ## read specific parts of lines
+        ## skip the first line and assign each line the name of the first string in the line "vlan1"
+        
+        ## https://www.youtube.com/watch?v=ZLCZkMk69y0 how to read a line
+      
+        lines = file.readlines()
+
+        total_lines = len(lines)
+        print("there's a total of", str(total_lines), "file lines.")
+        line_number = int(input("Enter the line # that you'd like to read. Line: "))
+
+        if(line_number > total_lines):
+            print("There's a total of", str(total_lines), "file lines.")
+            print("The line of the text file is outside the range of actual lines in the file.")
+        else:
+            ## get a list of the strings of all lines, and remove any extra lines/newlines
+            line = lines[line_number - 1].rstrip('\n')
+            ## remove whitespaces from beginning or end
+            line = line.strip()  
+            print(line)
+
+
+'''
+         ##  experimental
+        interfaceRegexPattern = "^[a-zA-Z]+[0-0]"
+
+        pattern = re.compile(r'interface')
+        
+        matches = pattern.finditer(line)
+        for match in matches:
+            print(match)
+
+
+        ##print(interfaceRegexPattern in line_number)
+
+
+
+
+        ## print out all the line names, ask user which one they want to look at
+
+        print(file.read()) ## default is read all
+        print(file.read(2))  ## this means read this many characters
+
+        ## Ask whether they want a specific interface or all 
+        print("Would you like to review a specific interface or see all? If so, enter 1")
+        response = input(bool)
+        print("you entered", response)
+        
+
+        ## If they ask for all, print out the details of all
+        print(file.read())
+        ## If they specify a specific interface, print that one out
+        #else:
+        print(file.read(5))
+ 
         ## If they say quit, exit option 3. Otherwise go to the beginning of option 3.
         file.close()
+
+        '''
