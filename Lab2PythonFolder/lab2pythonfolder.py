d4b7511716844e4189a8099d4d40e298af727db6
VanessaDirner <vanessadirner.work@gmail.com>
2025-10-15 00:19:07 -0400

    more setup of flow logic for script

--- Lab2PythonFolder/lab2pythonfolder.py
+++ Lab2PythonFolder/lab2pythonfolder.py
@@ -3,67 +3,67 @@ from getpass import getpass
 
 quit = False
 
+rack = 7
+
+## save configs all separately because simplicity/not the focus of this LAb
+R1template = {
+    'device_type': 'cisco_ios',
+    'host':  '192.168.7.1',
+    'username':'cisco',
+    'password':'cisco'
+}
+
+R2template =  {
+    'device_type': 'cisco_ios',
+    'host':  '192.168.7.2',
+    'username':'cisco',
+    'password':'cisco'
+}
+
+R3template =  {
+    'device_type': 'cisco_ios',
+    'host':  '192.168.7.3',
+    'username':'cisco',
+    'password':'cisco'
+}
+
+SW1template = {
+    'device_type': 'cisco_ios',
+    'host':  '192.168.7.11',
+    'username':'cisco',
+    'password':'cisco'
+}
+
+SW2template =  {
+    'device_type': 'cisco_ios',
+    'host':  '192.168.7.12',
+    'username':'cisco',
+    'password':'cisco'
+}
+
+SW3template = {
+    'device_type': 'cisco_ios',
+    'host':  '192.168.7.13',
+    'username':'cisco',
+    'password':'cisco'
+}
+
+SW4template =  {
+    'device_type': 'cisco_ios',
+    'host':  '192.168.7.14',
+    'username':'cisco',
+    'password':'cisco'
+}
+
+SW5template =  {
+    'device_type': 'cisco_ios',
+    'host':  '192.168.7.15',
+    'username':'cisco',
+    'password':'cisco'
+}
+
 
 while quit != True:
-
-    rack = 7
-
-    R1template = {
-        'device_type': 'cisco_ios',
-        'host':  '192.168.7.1',
-        'username':'cisco',
-        'password':'cisco'
-    }
-
-    R2template =  {
-        'device_type': 'cisco_ios',
-        'host':  '192.168.7.2',
-        'username':'cisco',
-        'password':'cisco'
-    }
-
-    R3template =  {
-        'device_type': 'cisco_ios',
-        'host':  '192.168.7.3',
-        'username':'cisco',
-        'password':'cisco'
-    }
-
-    SW1template = {
-        'device_type': 'cisco_ios',
-        'host':  '192.168.7.11',
-        'username':'cisco',
-        'password':'cisco'
-    }
-
-    SW2template =  {
-        'device_type': 'cisco_ios',
-        'host':  '192.168.7.12',
-        'username':'cisco',
-        'password':'cisco'
-    }
-
-    SW3template = {
-        'device_type': 'cisco_ios',
-        'host':  '192.168.7.13',
-        'username':'cisco',
-        'password':'cisco'
-    }
-
-    SW4template =  {
-        'device_type': 'cisco_ios',
-        'host':  '192.168.7.14',
-        'username':'cisco',
-        'password':'cisco'
-    }
-
-    
-    SW5template =  {
-        'device_type': 'cisco_ios',
-        'host':  '192.168.7.15',
-        'username':'cisco',
-        'password':'cisco'
-    }
 
     ##Identify what Rack you are in within the prompt
     print("You are at rack ", rack)
@@ -72,76 +72,121 @@ while quit != True:
     print("Hi, which device would you like to connect to. Your options are: R1, R2, R3, SW1, SW2, SW3, SW4, SW5")
 
     devicechoice = input()
+    selecteddevice = ''
 
     ##print("now connecting to machine.", devices[1])
     ##Validate the IP Address entered works for your Rack or series of devices
-    print("You selected: ", devicechoice)
-    if (devicechoice == "quit"):
-        quit = True
-    elif(devicechoice == "R1" ):
-        print("You selected R1")
-        print("loading configuration..", R1template)
-    elif(devicechoice == "R2"):
-        print("You selected R2")
-        print("loading configuration..", R2template)
-    elif(devicechoice == "R3" ):
-        print("You selected R3")
-        print("loading configuration..", R3template)
-    elif(devicechoice == "SW1"):
-        print("You selected SW1")
-        print("loading configuration..", SW1template)
-    elif(devicechoice == "SW2"):
-        print("You selected SW2")
-        print("loading configuration..", SW2template)
-    elif(devicechoice == "SW3"):
-        print("You selected SW3")
-        print("loading configuration..", SW3template)
-    elif(devicechoice == "SW4"):
-        print("You selected SW4")
-        print("loading configuration..", SW4template)
-    elif(devicechoice == "SW5"):
-        print("You selected SW5")
-        print("loading configuration..", SW5template)
-    else:
-        print("You didn't select one of the options. Try again")
-
- 
-    '''
+    try:
+        print("You entered: ", devicechoice , "succesfully")
+        if (devicechoice == "quit"):
+            quit = True
+        elif(devicechoice == "R1" ):
+            print("You selected R1")
+            print("loading configuration..", R1template, "succesfully")
+            selecteddevice = R1template
+        elif(devicechoice == "R2"):
+            print("You selected R2")
+            print("loading configuration..", R2template, "succesfully")
+            selecteddevice = R2template
+        elif(devicechoice == "R3" ):
+            print("You selected R3")
+            print("loading configuration..", R3template, "succesfully")
+            selecteddevice = R3template
+        elif(devicechoice == "SW1"):
+            print("You selected SW1")
+            print("loading configuration..", SW1template, "succesfully")
+            selecteddevice = SW1template
+        elif(devicechoice == "SW2"):
+            print("You selected SW2")
+            print("loading configuration..", SW2template, "succesfully")
+            selecteddevice = SW2template
+        elif(devicechoice == "SW3"):
+            print("You selected SW3")
+            print("loading configuration..", SW3template, "succesfully")
+            selecteddevice = SW3template
+        elif(devicechoice == "SW4"):
+            print("You selected SW4")
+            print("loading configuration..", SW4template, "succesfully")
+            selecteddevice = SW4template
+        elif(devicechoice == "SW5"):
+            print("You selected SW5")
+            print("loading configuration..", SW5template, "succesfully")
+            selecteddevice = SW5template
+        else:
+            print("You didn't select one of the options. Try again")
+    except:
+        print("Failed to get details about device choice.")
+    
+    
+  
     ##Connect to the Network Device, and display the following information
     net_connect = ConnectHandler(**ciscotemplate)
     net_connect.enable()
     print(net_connect.find_prompt())
+
     ##Display the active Hostname of the Device
     hostname = net_connect.send_command('hostname')
-    ##Display the Model of the Device - show version - a specific line has the model type in it, pip command only get only that
+
+    ##Display the Model of the Device - show version - a specific line has the model type in it, pipe command only get only that
     showver = net_connect.send_command('show version | include Model Number')
     print(showver)
 
-    ipintbr = net_connect.send_command('show ip int brief')
-    print(ipintbr)
-    ospfneighbor = net_connect.send_command('show ip ospf neighbor')
-    print(ospfneighbor)
-    runconf = net_connect.send_command('show running-config | include hostname')
-    print(runconf)
+    print("displayed basic device information above succesfully. What other details would you like to see? ")
+    
+
+    #Prompt user to save the following information to appropriately named files
+    #Interface information (show ip interface brief)
+    #OSPF Neighbor Information (show ip ospf neighbor)
+    #Running configuration file (show running-config)
+
+    outoption = input()
+
+    try:
+        if outoption == 1:            
+            print("You selected 1 succesfully.")
+            ipintbr = net_connect.send_command('show ip int brief')
+            print(ipintbr)
+            print("Printed details succesfully.")
+            try:
+                print("now saving ipintbr to a file")
+                with open("ipintbr.txt", "w") as f:
+                    f.write(ipintbr)
+            except:
+                print("Failed to print to file")
+        elif outoption == 2:
+            print("You selected 1 succesfully.")
+            ospfneighbor = net_connect.send_command('show ip ospf neighbor')
+            print(ospfneighbor)
+            print("Printed details succesfully.")
+            try:
+                print("now saving ospfneighbor to a file")
+                with open("ospfneighbor.txt", "w") as f:
+                    f.write(ospfneighbor)
+            except:
+                print("Failed to print to file")
+        elif outoption == 3:
+            print("You selected 1 succesfully.")
+            runconf = net_connect.send_command('show running-config | include hostname')
+            print(runconf)
+            print("Printed details succesfully.")
+            try:
+                print("now saving runconf to a file")
+                with open("runconf.txt", "w") as f:
+                    f.write(runconf)
+            except:
+                print("Failed to print to file")
+        else:
+            print("Sorry, was looking for 1, 2 or 3 for selecting a cisco device output")
+    except:
+        print("Failed to read input.")
+
+
     net_connect.disconnect()
-    '''
-
-    '''
-    Prompt user to save the following information to appropriately named files
-
-    Interface information (show ip interface brief)
-
-    OSPF Neighbor Information (show ip ospf neighbor)
-
-    Running configuration file (show running-config)
-    '''
-    '''
 
 
-    All completed prompts should loop back to section b, and offer the user an option to quit if they are satisfied with their exports.
+    #All completed prompts should loop back to section b, and offer the user an option to quit if they are satisfied with their exports.
 
-    A success or failure message should accompany any task.
-    '''
+    #A success or failure message should accompany any task.
 
 
 if quit == True:
