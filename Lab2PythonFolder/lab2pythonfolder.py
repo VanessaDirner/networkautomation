cf2601ccec6fd759bb9f258a6534d29a239ed1d0
VanessaDirner <vanessadirner.work@gmail.com>
2025-10-14 22:30:55 -0400

    setting up basic connection

--- Lab2PythonFolder/lab2pythonfolder.py
+++ Lab2PythonFolder/lab2pythonfolder.py
@@ -1,18 +1,47 @@
-import netmiko
+from netmiko import ConnectHandler
+from getpass import getpass
+
+
+devices = {'R1':'192.168.7.1', 'R2':'192.168.7.2', 'R3':'192.168.7.3', 'SW1':'192.168.7.11', 'SW2':'192.168.7.12', 'SW3':'192.168.7.13', 'SW4':'192.168.7.14','SW5':'192.168.7.15'}
+
+rack = 7
+
+ciscotemplate = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.7.11',
+    'username':'cisco',
+    'password':'cisco'
+}
+
+##Prompt a User to input an IP Address for a Network Device in the Rack
+print("Hi, which device woud you like to connect to. Your options are: ", devices)
+
+##Validate the IP Address entered works for your Rack or series of devices
+
+##Identify what Rack you are in within the prompt
+print("You are at rack ", rack)
+
+##Connect to the Network Device, and display the following information
+net_connect = ConnectHandler(**ciscotemplate)
+net_connect.enable()
+print(net_connect.find_prompt())
+
+##Display the active Hostname of the Device
+hostname = net_connect.send_command('hostname')
+##Display the Model of the Device - show version - a specific line has the model type in it, pip command only get only that
+##showver = net_connect.send_command('show version')
+##print(showver)
+
+##ipintbr = net_connect.send_command('show ip int brief')
+##print(ipintbr)
+ospfneighbor = net_connect.send_command('show ip ospf neighbor')
+print(ospfneighbor)
+runconf = net_connect.send_command('show running-config')
+
+net_connect.disconnect()
+
 
 '''
-Prompt a User to input an IP Address for a Network Device in the Rack
-
-Identify what Rack you are in within the prompt
-
-Validate the IP Address entered works for your Rack or series of devices
-
-Connect to the Network Device, and display the following information
-
-Display the active Hostname of the Device
-
-Display the Model of the Device
-
 Prompt user to save the following information to appropriately named files
 
 Interface information (show ip interface brief)
@@ -20,8 +49,19 @@ Interface information (show ip interface brief)
 OSPF Neighbor Information (show ip ospf neighbor)
 
 Running configuration file (show running-config)
+'''
+'''
+
 
 All completed prompts should loop back to section b, and offer the user an option to quit if they are satisfied with their exports.
 
 A success or failure message should accompany any task.
 '''
+
+
+##devicechoice = input()
+
+##if devicechoice == devices[1]:
+##    print(devices[1])
+
+##print("now connecting to machine.", devices[1])
