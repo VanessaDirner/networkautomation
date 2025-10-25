29ba902f738340a871c0152554ed941ce0924e45
VanessaDirner <vanessadirner.work@gmail.com>
2025-10-24 20:13:44 -0400

    can get input of interface

--- Lab2/main.py
+++ Lab2/main.py
@@ -7,7 +7,8 @@ adapters = ifaddr.get_adapters()
 
 ## send to dictionary to choose from later
 adapters_dict = {}
-count = 0
+count = int(0)
+print(type(count))
 
 for adapter in adapters:
     print("ips of network adapter", adapter.nice_name)
@@ -19,14 +20,21 @@ for adapter in adapters:
 
 print(adapters_dict)
 
-'''
+
 print("choose an adapter to check devices on")
 
-adpt_choice = input()
+adpt_choice = int(input())
+print("You chose 2")
 
+try:
+    if adpt_choice >= 0 and adpt_choice <= count:
+        print("Will now attempt to enumerate network devices")
+    else:
+        print("Sorry, that was outside the range of interfaces you have. Please double check the given list.")
+except:
+    print("Sorry that was not a number. ")
 
-
-
+'''
 try:
     pings = ping('10.1.1.1')
     print("pinging", pings)
