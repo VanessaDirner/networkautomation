adcdc418d6a5c1c9c676183bd9a39eb5c189a0f0
VanessaDirner <vanessadirner.work@gmail.com>
2025-10-24 21:42:19 -0400

    trying to figure out without any formatting just ips

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,7 +1,10 @@
 import os
 from ping3 import ping, verbose_ping
 import ifaddr
-from ipaddress import ip_network
+import ipaddress
+from ipaddress import IPv4Network, ip_network
+import time
+import json
 
 ## get all adapters
 adapters = ifaddr.get_adapters()
@@ -32,7 +35,7 @@ for adapter in adapters:
 
 print(adapters_dict)
 print(ips_dict)
-'''
+
 print("choose an adapter to check devices on")
 exit()
 adpt_choice = int(input())
@@ -46,19 +49,27 @@ try:
         print("Sorry, that was outside the range of interfaces you have. Please double check the given list.")
 except:
     print("Sorry that was not a number. ")
-'''
+
 ## get ip details of the interface
 
 range = ip_network('192.168.7.0/24')
 
-address = list(range.hosts())
-print(address)
+addresses = list(range.hosts())
+print(addresses)
+
 
 ## ping all addresses in space
-for address in range:
-    try:
-        pings = verbose_ping(address)
-        print("pinging", pings)
-    except:
-        print("unknown error")
+#for address in addresses:
+#        print(address)
+#        time.sleep(15)
 
+for addr in IPv4Network('192.168.7.0/24'):
+    ip = print(addr)
+    ping(ip)
+
+
+ip = ipaddress.IPv4Address('192.168.1.1')
+print(ip - 1)
+
+#ip_list = [str(ip) for ip in IPv4Network('192.168.8.0/24')]
+#print(ip_list[:5])
