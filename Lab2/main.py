22a40865c291e4b65f3ac202d4899d12f6b6e06e
VanessaDirner <vanessadirner.work@gmail.com>
2025-10-26 18:41:35 -0400

    more nice formatting

--- Lab2/main.py
+++ Lab2/main.py
@@ -9,15 +9,59 @@ import scapy
 
 ## get all adapters
 adapters = ifaddr.get_adapters()
-print(adapters)
-print(type(adapters))
-print(adapters[2])
-print(adapters[2].ips)
-print(adapters[2].ips[0])
-print(adapters[2].ips[0].ip)
-print(adapters[2].ips[0].network_prefix)
-print(adapters[2].ips[0].nice_name)
+
+## print details nicely
+for adapter in adapters:
+    print(adapter.nice_name) 
+    for ip in adapter.ips:
+        print(ip.ip)
+        print(ip.network_prefix)
+        print(ip.nice_name)
+
+
+valid = False
+tries = 0
+## double check their answer to see if we can select an interface based on it
+while tries < 3 and valid == False:
+    try:
+        print("choose an adapter to check devices on")
+        adpt_choice = int(input())
+        print("You chose ", adpt_choice)
+        if adpt_choice > 0 and adpt_choice <= len(adapters):
+            print("Will now attempt to enumerate network devices for ", adapters[adpt_choice].nice_name)
+            print("the details of that adapter are: ")
+            print(adapters[adpt_choice].ips[0].ip)
+            print(adapters[adpt_choice].ips[0].network_prefix)
+            print(adapters[adpt_choice].ips[0].nice_name)
+            valid = True
+        else:
+            tries = tries + 1
+            print("Sorry, that was outside the range of interfaces you have. Please double check the given list.", tries)
+
+    except:
+        tries = tries + 1
+        print("Sorry that was not a number. ", tries)
+
+
+
+
+
 exit()
+while tries < 3 and valid == False:
+    try:
+        print("which network on that adapter would you like to choose?")
+        print("your options are ", adapters[adpt_choice])
+        print("Enter a number between ", len(adapters[adpt_choice]))
+        input_net = input()
+        if input_net > -1 and input_net <= len(adapters[adpt_choice].ips):
+            print("you selected", adapters[adpt_choice].ips[input_net])
+        valid = True
+    except:
+        print("Sorry that's not a valid network adapter")
+        
+
+
+
 ## send to dictionaries to choose from later
 adapters_dict = {}
 ips_dict = {}
@@ -128,3 +172,15 @@ d = ping('192.168.7.15')
 print(a,b,c,d)
 
 exit()
+
+
+'''
+print(type(adapters))
+print(adapters[2])
+print(adapters[2].ips)
+print(adapters[2].ips[0])
+print(adapters[2].ips[0].ip)
+print(adapters[2].ips[0].network_prefix)
+print(adapters[2].ips[0].nice_name)
+
+'''
