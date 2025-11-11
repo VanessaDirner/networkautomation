9adb0507f8ec19fda64417f9957b13c250cf4813
VanessaDirner <vanessadirner.work@gmail.com>
2025-11-10 21:34:34 -0500

    starting code

--- assignment3/main.py
+++ assignment3/main.py
@@ -1,12 +1,28 @@
+# Identify what Rack you are in within the prompt
+print("you are at rack 7")
 
 # Prompt a User to input an IP Address for a Router in the Rack
 print("Enter an IP Address for a Router in the Rack")
 
-# Identify what Rack you are in within the prompt
-print("you are at rack 7")
-
 # Validate the IP Address entered works for your Rack or series of devices
-print("that is a valid IP address")
+valid_devices = ['192.168.7.1', '192.168.7.2']
+device_input = input()
+
+counter = int()
+valid = False
+
+while valid == False:
+    for x in valid_devices:
+        if device_input == valid_devices[counter]:
+            print("You selected", valid_devices[counter])
+            print("that is a valid IP address")
+            valid = True
+        else:
+            if valid == False:
+                print("still looking for a match, latest option was ", valid_devices[counter])
+        counter = counter + 1
+    
+exit()
 
 # Connect to the Network Device, and display the following information
 print("Now connecting to the Router")
