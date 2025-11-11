2f728ae3fad9e8096d1450e71f1e068e9f1c6435
VanessaDirner <vanessadirner.work@gmail.com>
2025-11-10 22:43:39 -0500

    sorting out connection template

---
+++ assignment3/connectandrun.py
@@ -0,0 +1,89 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+import os
+import json
+
+
+def runaction(action, host_ip, template):
+        
+    # Connect to the Network Device, and display the following information
+    print("Now connecting to the Router")
+    print("Connected succesfully")
+
+    # Display the active Hostname of the Device
+    print("The hostname is ")
+    print("Printed hostname succesfully")
+
+    # Display the Model of the Device
+    print("The model is ")
+    print("printed model succesfully")
+
+    # Prompt user to pick one of the following options to have it be performed on the device you are connecting to:
+    print("Which option would you like to perform.")
+    print("Option 1: Create a Loopback Interface with a valid IP address")
+    print("Option 2: Enter in a static route to another Router in your setup")
+    print("Option 3: Display and validate the IP Route Table is correct")
+
+
+
+
+    # All completed prompts should prompt the user to continue modifying the currently selected device,
+    #  and then loop back to section b,
+    #  and always offer the user an option to quit if they are satisfied with their actions.
+
+    print("Would you like to continue modifying the current device?")
+    print("Enter 1 to continue, 0 to stop.")
+
+    ## next part 
+    print("filename is ", template)
+    f = open(template, 'r')
+    data = json.load(f)
+    print(data)
+    print("your connection details are", data , "for device", host_ip)
+    debug_a = net_connect = ConnectHandler(**data)
+    print(debug_a)
+    net_connect.enable() 
+    print(net_connect.find_prompt())
+    success = False
+
+    print("action is ", action, "with a type of ", type(action))
+    ## once connected, run command
+    if action == "0":
+        try:    
+            # Create a Loopback Interface with a valid IP address
+            print("Creating a Loopback Interface with a valid IP address")
+            #connect_success = True
+            runconf = net_connect.send_command('')
+            net_connect.disconnect()
+        except Exception as e:
+            if success == True:
+                print("Device reload completed")
+            else:
+                print("Failed to reload device: ", e)
+    if action == "1":
+        try:
+            runconf = net_connect.send_command('')
+            print("Grabbed details succesfully.")
+            # Enter in a static route to another Router in your setup
+            print("Entering in a static route to another Router in your setup")
+            print("host IP is", host_ip)
+            filename = f"C:\\Users\\Vanessa\\Documents\\GitHub\\networkautomation\\Lab2\\runningconfig\\{host_ip}.txt"
+            print("filename will be", filename)
+            with open(filename, "w") as f:
+                f.write(runconf)
+            print("Saved details to file succesfully")
+            success = True
+        except Exception as e:
+            print("Failed to print to file:", e)
+    if action == '2':
+        # Display and validate the IP Route Table is correct
+        print("Displaying and validating that the IP Route Table is correct")
+    if action == '3':
+        # Save your configuration to an appropriately named file.
+        print("Saving your configuration to an appropriately named file.")
+    if success == True:
+        print("") # fail succesfully and silently -- what a great idea for prod
+    else:
+         print("did not run an action")
+
+
--- assignment3/main.py
+++ assignment3/main.py
@@ -1,3 +1,5 @@
+import connectandrun
+
 # Identify what Rack you are in within the prompt
 print("you are at rack 7")
 
@@ -6,6 +8,21 @@ print("Enter an IP Address for a Router in the Rack")
 
 # Validate the IP Address entered works for your Rack or series of devices
 valid_devices = ['192.168.7.1', '192.168.7.2']
+
+'192.168.7.1' = {
+    'device_type': 'cisco_ios',
+    'host':  '192.168.7.1',
+    'username':'cisco',
+    'password':'cisco'
+}
+'192.168.7.2' =  {
+    'device_type': 'cisco_ios',
+    'host':  '192.168.7.2',
+    'username':'cisco',
+    'password':'cisco'
+}
+                
+
 device_input = input()
 
 counter = int()
@@ -24,41 +41,4 @@ while valid == False:
     
 exit()
 
-# Connect to the Network Device, and display the following information
-print("Now connecting to the Router")
-print("Connected succesfully")
-
-# Display the active Hostname of the Device
-print("The hostname is ")
-print("Printed hostname succesfully")
-
-# Display the Model of the Device
-print("The model is ")
-print("printed model succesfully")
-
-# Prompt user to pick one of the following options to have it be performed on the device you are connecting to:
-print("Which option would you like to perform.")
-print("Option 1: Create a Loopback Interface with a valid IP address")
-print("Option 2: Enter in a static route to another Router in your setup")
-print("Option 3: Display and validate the IP Route Table is correct")
-
-# Create a Loopback Interface with a valid IP address
-print("Creating a Loopback Interface with a valid IP address")
-
-# Enter in a static route to another Router in your setup
-print("Entering in a static route to another Router in your setup")
-
-# Display and validate the IP Route Table is correct
-print("Displaying and validating that the IP Route Table is correct")
-
-# Save your configuration to an appropriately named file.
-print("Saving your configuration to an appropriately named file.")
-
-# All completed prompts should prompt the user to continue modifying the currently selected device,
-#  and then loop back to section b,
-#  and always offer the user an option to quit if they are satisfied with their actions.
-
-print("Would you like to continue modifying the current device?")
-print("Enter 1 to continue, 0 to stop.")
-
-# A success or failure message should accompany any task.
+connectandrun.runaction()
